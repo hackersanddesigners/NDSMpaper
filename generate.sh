dd6306ca5a9c041a406f2a75736e2d5f90136d43
hrk <heerko@swummoq.net>
2019-09-20 14:45:51 +0200

    Generate the font styles on the fly. Allow for regeneration of PDF from existing html

--- generate.sh
+++ generate.sh
@@ -1,37 +1,46 @@
 #! /usr/bin/env python
+import os, sys, getopt, random, subprocess, pathlib, math, argparse, shutil, time, re
 from weasyprint import HTML
-import os, sys, getopt, random, subprocess
-import pathlib
 from bs4 import BeautifulSoup
-import argparse
-import shutil
 from shutil import copyfile
-import time
 from subprocess import call
 
 cwd =  pathlib.Path.cwd()
-pathlib.Path( cwd / 'srcdocs' ).mkdir( parents=True, exist_ok=True )
+# pathlib.Path( cwd / 'srcdocs' ).mkdir( parents=True, exist_ok=True )
 src_path = cwd / 'srcdocs'
 pathlib.Path( cwd / 'build' / 'clean' ).mkdir( parents=True, exist_ok=True )
 dest_path = cwd / 'build' / 'clean'
+fromHTML = None
 
 def main(argv):
+    global src_path, fromHTML
+
     timestr = time.strftime( "%Y-%m%d-%H%M%S" )
     # filename = "output/hdsa%s.mp4" %
 
     parser = argparse.ArgumentParser(description='Generate H&D Book')
     parser.add_argument('-c','--clean', help='Cleanup input HTML files. Removes unwanted html & css. Saves files in srcdocs/clean', action='store_true' )
     parser.add_argument('-b','--build', help='Combines the files in build/clean (created by running this script with -c option) and creates a PDF', action='store_true' )
-    parser.add_argument('-o','--output', help='Output pdf filename. Default: book.pdf', default="book-" + timestr + ".pdf" )
+    parser.add_argument('-o','--output', help='Output filename (without extension). Default: book.pdf', default="book-" + timestr )
+    parser.add_argument('-i','--input', help='Input folder containing the html files', default="srcdocs" )
+    parser.add_argument('--html', help='Input html file (stored in /build)' )
     args = parser.parse_args()
 
-    if args.build:
+    if args.input:
+        src_path = cwd / args.input
+
+    if args.html:
+        fromHTML = args.html
+
+    if args.build or args.html:
         build( args.output )
     elif args.clean:
         cleanFiles()
 
+
 def cleanFiles():
     shutil.rmtree(dest_path)
+    print( "Reading documents from %s " % src_path )
     pathlib.Path( cwd / 'build' / 'clean' ).mkdir( parents=True, exist_ok=True )
     for i, file in enumerate( sorted( src_path.rglob( '*.html' ) ) ):
         with open( file, 'r' ) as src_file:
@@ -75,33 +84,41 @@ def cleanFiles():
 
 
 def build( output_filename ):
-    template = pathlib.Path.cwd() / 'lib' / 'template.html'
-    with open( template, 'r' ) as book:
-        content = book.read()
-    book_html = BeautifulSoup( content, 'html.parser' )
-    container = book_html.body.div
+    if not fromHTML:
+        template = pathlib.Path.cwd() / 'lib' / 'template.html'
+        with open( template, 'r' ) as book:
+            content = book.read()
+        book_html = BeautifulSoup( content, 'html.parser' )
+        container = book_html.body.div
 
-    style = ''
-    for j in range( 0, 10) :
-        for i in range( 0, 100) :
-            style += r'article:nth-of-type({}) .article-rnd p:nth-of-type({}){{ padding-left: {}%; }}'.format(j, i, random.randint(0,50) )
-    style_tag = book_html.new_tag( "style" )
-    style_tag.string = style;
-    book_html.body.insert( 0, style_tag )
+        body_fonts = createFontStyles( 'body' )
+        heading_fonts = createFontStyles( 'heading' )
 
-    for idx, file in enumerate( sorted( dest_path.rglob( '*.html' ) ) ):
-        with open( file, 'r' ) as src_file:
-            content = src_file.read()
-            body = formatDocument( content, idx )
-            body[ 'class' ] = "article-" + str( idx )
-            # body[ 'style' ] = 'page: article-' + str( idx )
-            container.append( body )
+        style = body_fonts + heading_fonts
+        for j in range( 0, 10) :
+            for i in range( 0, 100) :
+                style += r'article:nth-of-type({}) .article-rnd p:nth-of-type({}){{ padding-left: {}%; }}\n'.format(j, i, random.randint(0,50) )
+        style_tag = book_html.new_tag( "style" )
+        style_tag.string = style;
+        book_html.body.insert( 0, style_tag )
 
-    dest_file = pathlib.Path.cwd() / 'build' / 'book.html'
-    with open( dest_file, 'w' ) as output:
-        output.write( book_html.prettify() )
+        for idx, file in enumerate( sorted( dest_path.rglob( '*.html' ) ) ):
+            with open( file, 'r' ) as src_file:
+                content = src_file.read()
+                body = formatDocument( content, idx )
+                body[ 'class' ] = "article-" + str( idx )
+                # body[ 'style' ] = 'page: article-' + str( idx )
+                container.append( body )
 
-    pdf_path = pathlib.Path.cwd() / 'build' / output_filename
+        dest_file = str( pathlib.Path.cwd() / 'build' / output_filename ) + '.html'
+        with open( dest_file, 'w' ) as output:
+            output.write( book_html.prettify() )
+    else:
+        dest_file = pathlib.Path.cwd() / 'build' / fromHTML
+        print( "PDF from preexisting html: %s" % str( dest_file ) )
+
+
+    pdf_path = str( pathlib.Path.cwd() / 'build' / output_filename ) + ".pdf"
     print( str( pdf_path ) )
 
     HTML( dest_file ).write_pdf( pdf_path )
@@ -111,8 +128,6 @@ def build( output_filename ):
     # from PdfGenerator import PdfGenerator
     # pdf = PdfGenerator( str( HTML( dest_file ) ) )
     # pdf.render_pdf()
-
-import re
 
 def formatDocument( content, idx ):
     soup = BeautifulSoup( content, 'html.parser' )
@@ -125,17 +140,21 @@ def formatDocument( content, idx ):
     if random.randint(0,4) == 0:
         body[ 'class' ] = 'article-rnd'
     else:
-        body[ 'class' ] = 'columns cols-' + str( random.randint( 2, 6 ) ) + ' bodyfont-' + str( random.randint( 1, 5 ) )
+        body[ 'class' ] = 'bread columns cols-' + str( random.randint( 2, 6 ) ) + ' bodyfont-' + str( random.randint( 1, 5 ) )
 
     # scoped does work in Weasyprint fortunately so we add an id to the article and suffix the css
     body[ 'id' ] = 'article-' + str( idx )
     for header in body.select( 'h1, h2' ):
         rndHeadingFont( header )
+    for h2 in body.select( 'h2' ):
+        wrapper.insert( 0, h2 ) # place the h1 outside of the article so it can be fullwidth
     for h1 in body.select( 'h1' ):
         wrapper.insert( 0, h1 ) # place the h1 outside of the article so it can be fullwidth
     for img in soup.select( 'img' ):
         img[ 'src' ] = 'clean/' + img[ 'src' ] # adjust image path
-    rndDots( soup, idx )
+
+    # rndDots( soup, idx )
+    rndLogos( soup, idx )
 
     # get the style tag from head and turn it into a style scoped to the article
     # this isnt going to be pretty...
@@ -162,6 +181,29 @@ def formatDocument( content, idx ):
 
     return wrapper
 
+def createFontStyles( dirname ):
+    path = pathlib.Path.cwd() / 'lib/fonts/' / dirname
+    fonts = getFiles( path, 'woff' )
+    font_list = []
+    style_str = ""
+    for i, font in enumerate( fonts ):
+        base = os.path.basename( font )
+        name = os.path.splitext( base )[ 0 ]
+        font_list.append( name )
+        print( dirname + " > " + base )
+        style = """
+        @font-face {{
+            font-family: '{0}';
+            src: url('../lib/fonts/{2}/{0}.woff') format('woff');
+        }}
+        .{2}font-{1}{{
+            font-family: '{0}' !important;
+        }}
+        """.format( name, i, dirname )
+        style_str += style
+    return style_str
+
+
 def rndDots( soup, idx ):
     # pick some random paragraph to :before the dots to
     i = 0
@@ -169,7 +211,7 @@ def rndDots( soup, idx ):
     picked = random.choices( ps, k=12 ) # the k is a bit of a magic number
     for p in picked:
         p[ 'class' ] = 'hasdot';
-        style = r'.article-{} p.hasdot:nth-of-type({}):before{{ left: {}%; top: {}%; }}'.format( idx, i, random.randint(5,95),  random.randint(5,95) )
+        style = r'#article-{} p.hasdot:nth-of-type({}):before{{ left: {}%; top: {}%; }}'.format( idx, i, random.randint(-30,130),  random.randint(0,100) )
         # style = ".article-"+str(idx)+" p.hasdot:nth-of-type(" + str(i) +"):before{ left: " + str( random.randint(5,95) ) + "%; top: " + str( random.randint(5,95) ) + "%; }"
         style_tag = soup.new_tag("style")
         style_tag[ 'scoped' ] = 'scoped'
@@ -177,12 +219,37 @@ def rndDots( soup, idx ):
         p.insert( 0, style_tag )
         i += 1
 
+def rndLogos( soup, idx ):
+    # pick some random paragraph to :before the dots to, to make sure that
+    # if an article spans multiple pages all pages (potentially) have some logos
+    ps = soup.select( 'p' )
+    logos = getFiles( pathlib.Path.cwd() / 'lib/logos', 'png' )
+    num_items = int( len( ps ) / 4 )
+    picked = random.choices( ps, k = num_items )
+
+    for p in picked:
+        i = ps.index( p )
+        p[ 'class' ] = 'haslogo logo-' + str( i ); # class to target with css
+        logo = random.choice( logos )
+        style = r'.article-{} .logo-{}:before{{ background-image: url({}); left: {}%; top: {}%; }}'.format( idx, i, logo, random.randint(-30,130),  random.randint(0,100) )
+        style_tag = soup.new_tag("style")
+        # style_tag[ 'scoped' ] = 'scoped'
+        style_tag.string = style;
+        p.insert( 0, style_tag )
+
 def rndHeadingFont( header ):
     header[ 'class' ] = "headingfont-" + str( random.randint( 1, 17 ) )
     return header
 
 def generateDotCss():
     str = ""
+
+# gets all files with .ext from a directory
+def getFiles( path, ext ):
+    files = []
+    for i, file in enumerate( sorted( path.glob( '*' + ext ) ) ):
+        files.append( file )
+    return files
 
 if __name__ == "__main__":
    main( sys.argv[ 1: ] )
--- readme.md
+++ readme.md
@@ -42,16 +42,19 @@ Google docs places a lot of representational css inline in the document.
 This command cleans most of that up. We only leave the font-weight and font-style
 rules because we actually need those.
 The cleaned documents will be placed in ./build/clean. Adjust those as needed.
+By default is searches for files in the .srcdocs folder
 
 ``` python generate.py --clean ```
 
+Or specify source folder:
+```python generate.sh --clean --input ./srcdocs_en```
 
 Run build command
 ---
 This will combine all the files in ./build/clean by getting the contents of the
 document body and wrapping that in an &lt;article&gt; tag.
 The combined document will be saved as  ./build/book.html and this file is used
-by Weasyprint to generate ./build/book.pdf
+by Weasyprint to generate ./build/book-<date>.html and ./build/book-<date>.pdf
 
 ``` python generate.py --build ```
 
@@ -60,6 +63,20 @@ by Weasyprint to generate ./build/book.pdf
 Set the output filename by adding --output [filename]
 
 ``` python generate.py --build --output ndsm_papger.pdf ```
+
+Create PDF from preexisting html
+---
+This option allow for small text changes. To do that edit the generated
+/build/book-<date>.html and pass that to the --html flag
+
+```python generate.sh --html book-2019-0920-143629.html```
+
+Examples
+---
+Clean the input html and generate a Pdf
+
+```python generate.sh --clean --input ./srcdocs_en && python generate.sh --build```
+
 
 ## Resources
 https://www.w3.org/TR/css-page-3/#cascading-and-page-context
